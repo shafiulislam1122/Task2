bf108e4aa702bdbef68bf1fc19d74799d237725b
shafiulislam1122 <98739731+shafiulislam1122@users.noreply.github.com>
2022-02-23 15:02:38 +0600

    Update task.php

--- Task2/task.php
+++ Task2/task.php
@@ -1,55 +1,59 @@
-
-
 <html>
 <head>
-<title>
-My site
-</title>
-
+<h1>Registration Form</h1>
 </head>
 <body>
-    <h1>hello</h1>
-
-    <form action="Control/results.php" method="post">
-    <table>
-    <tr>
-    <td>First Name: </td>
-            <td>
-    Userfirstname  <input type ="text" name="ufirstname" placeholder>
-    </td>
-</tr>
-
-<tr>
-    <td>Last Name: </td>
-            <td>
-    Userlastname  <input type ="text" name="ulastname" placeholder>
-    </td>
-</tr>
-
-<tr>
-    <td>Age: </td>
-            <td>
-    Useragee  <input type ="text" name="uage" placeholder>
-    </td>
-</tr>
-<tr>
-    <td>First Name: </td>
-            <td>
-            password  <input type ="password" name="password"placeholder>
-    </td>
-</tr>
-    password  <input type ="password" name="password">
-
-
-    select a radio:
-   
-    <input type="radio" name="r1" value="radio1">Radio 1
-    <input type="radio" name="r1" value="radio2">Radio 2
-
-
-    <input type="submit" name="submit">
-</form>
+	<hr>
+	<form action= "Control/result.php" method ="post">
+	    <table>
+			<tr>
+			    <td>FirstName:</td>
+		        <td><input type="text" name="FirstName"> </td>  
+	     	</tr>
+			<tr>
+			    <td>LastName:</td>
+		        <td><input type="text" name="LastName"> </td>  
+		    </tr>
+				<tr>
+			    <td>Age:</td>
+		        <td><input type="number" name="Age"> </td>  
+		    </tr>   
+			<tr>
+			    <td>Email:</td>
+		        <td><input type="Email" name="Email">  </td>
+		    </tr> 
+		    <tr>
+			    <td>Password:</td>
+		        <td><input type="Password" name="Password">  </td>
+		    </tr> 
+		    <tr>
+			    <td>Designation:</td>
+		        <td>
+		        	<input type="radio" name="Designation" value="junior Programmer">juniorProgrammer
+		        	<input type="radio" name="Designation"value="Senior Programmer">SeniorProgrammer
+		        	<input type="radio" name="Designation"value="Project Leader">Project Lead
+		        </td>
+		    </tr>
+		    <tr>
+		    	<td>
+		    		Prefferred language:
+		    	</td>
+		    	<td>
+		    		<input type="checkbox" name="pl1" value="JAVA">JAVA
+		    		<input type="checkbox" name="pl2" value="PHP">PHP
+		    		<input type="checkbox" name="pl3" value="C++">C++
+		    	</td>
+		    </tr>
+		    <tr>
+		    	<td>
+		    		please choose a file:
+		    	</td>
+		    	<td><input type="file" name="image"></td>
+		    </tr> 
+		</table>
+		<input type="submit" value="submit" >
+		<input type="reset" name="reset">		
+	</form>
+</hr>
 </body>
 </html>
-        
-
